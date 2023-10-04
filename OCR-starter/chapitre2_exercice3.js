9956a444c0570baf48e2409f5c30994f2f4c6470
Briol Alexandre <alexandre.briol@student.hepl.be>
2023-10-04 14:55:33 +0200

    Fin Chap 2 ex3 (Pas le 2 parce que pas d'énoncé)

--- OCR-starter/chapitre2_exercice3.js
+++ OCR-starter/chapitre2_exercice3.js
@@ -1,15 +1,17 @@
 // Permutation de variables
-
+let a = 2;
+console.log(a);
+a = 1;
+console.log(a);
 
 
 
 // Solution classique utilisant une variable temporaire
 
-
+let b=4+5;
+console.log(b);
 
 // Solution alternative valable uniquement pour des nombres
 /* nombre1 += nombre2;
 nombre2 = nombre1 - nombre2;
 nombre1 -= nombre2; */
-
-
