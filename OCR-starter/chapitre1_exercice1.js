cf3366e3266bd8ff178ad8c1cd32e87768ece001
Briol Alexandre <alexandre.briol@student.hepl.be>
2023-10-04 12:41:39 +0200

    Fin exo 009

--- OCR-starter/chapitre1_exercice1.js
+++ OCR-starter/chapitre1_exercice1.js
@@ -4,4 +4,7 @@ Affichez dans la console :
 - votre prénom (ce doit être une chaîne)
 - votre âge (ce doit être un nombre)
 */
-
+const name = 'Alexandre';
+const age = 20;
+console.log(name);
+console.log(age);
