bb9a2406b0c9f6b81e928416f4f2e1bea17b7d6c
Briol Alexandre <alexandre.briol@student.hepl.be>
2023-10-04 15:08:40 +0200

    Fin exo 010

--- starter/script010.js
+++ starter/script010.js
@@ -4,7 +4,9 @@
 
 // 1. Déclarer les variables now, ageJohn, ageMark, yearJohn et yearMark
 // puis affecter aux trois premières les valeurs 2019, 28 et 33
-
+const now = 2019;
+const ageJohn = 28;
+const ageMark = 33;
 
 // Opérateurs mathématiques
 
@@ -13,11 +15,14 @@
 // idem pour yearMark
 // puis afficher la date de naissance de John 
 // et "Mark est né en …"
-
-
+const yearJohn = now - ageJohn;
+const yearMark = now - ageMark;
+console.log(yearJohn);
+alert(`Mark est né en ${yearMark}`)
 // 3. Afficher l'année à laquelle nous serons dans deux ans
 // le double de l'année
 // le dixième de l'année
+console.log(now+2, now*10, now/10);
 
 
 
@@ -28,4 +33,4 @@
 
 
 // Opérateur typeof
-
+console.log(typeof now);
